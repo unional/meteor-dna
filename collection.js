6de4a4f783b8582adf9cc37f2dc73eac9851f548
Homa Wong <hwong@paloaltonetworks.com>
2014-05-04 09:27:30 -0700

    Interim.  

---
+++ collection.js
@@ -0,0 +1,6 @@
+Dna.Collection = function (type, options) {
+    var self = this;
+    if (!(self instanceof Dna.Collection)) {
+        throw new Error('use "new" to construct a Dna.Collection');
+    }
+};
--- package.js
+++ package.js
@@ -3,11 +3,13 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+    api.use(["standard-app-packages", "accounts-base"]);
+
     api.add_files(["dna.js"], ['client', 'server']);
     api.export(["Dna"], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use(["tinytest"]);
+    api.use(["standard-app-packages", "accounts-base", "tinytest"]);
     api.add_files(["dna.js"], ['client', 'server']);
 });
