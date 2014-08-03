fad6f6ce08643649f20372a9a6d037e7de89e23c
Homa Wong <hwong@paloaltonetworks.com>
2014-08-03 02:28:34 -0700

    Prepare to add tests

---
+++ packages/dna/datums-test.js
@@ -0,0 +1,10 @@
+/*
+ * Datum Network Architecture
+ * Copyright(c) 2014, Unional (https://github.com/unional)
+ * @license Licensed under the MIT License (https://github.com/unional/unional/LICENSE)).
+ * Created by hwong on 8/3/14.
+ */
+Tinytest.add("Dna.datums exsts", function(test) {
+    "use strict";
+    test.notEqual(undefined, Dna.datums, "Dna.datums should exist");
+});
--- packages/dna/package.js
+++ packages/dna/package.js
@@ -3,13 +3,13 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-    api.use(["standard-app-packages", "accounts-base"]);
+    api.use(["standard-app-packages"]);
 
     api.add_files(["dna.js", "datums.js", "contexts.js"], ['client', 'server']);
     api.export(["Dna"], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use(["standard-app-packages", "accounts-base", "tinytest"]);
-    api.add_files(["dna.js", "datums.js"], ['client', 'server']);
+    api.use(["tinytest"]);
+    api.add_files(["datums-test.js", "dna.js", "datums.js"], ['client', 'server']);
 });
